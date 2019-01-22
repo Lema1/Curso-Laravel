e26916ecb7fd9542782a475f38576a2060d7e5ae
Alvaro <contact@fastweb.com>
2019-01-22 17:08:56 -0300

    Paso 21

--- public/js/app.js
+++ public/js/app.js
@@ -48680,7 +48680,10 @@ Vue.component('example-component', __webpack_require__(/*! ./components/ExampleC
  */
 
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    menu: 0
+  }
 });
 
 /***/ }),
