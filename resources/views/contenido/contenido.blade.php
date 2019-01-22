d134f246a21680556a5bbc82f9f4eb03a1b713cf
Alvaro <contact@fastweb.com>
2019-01-22 20:21:12 -0300

    Paso 25 completado

--- resources/views/contenido/contenido.blade.php
+++ resources/views/contenido/contenido.blade.php
@@ -1,10 +1,10 @@
 @extends('index')
 @section('contenido')
     <template v-if="menu==0">
-        <example-component></example-component>    
+        <h1>Contenido menu 0</h1>    
     </template>
     <template v-if="menu==1">
-        <h1>Contenido menu 1</h1>
+        <categoria></categoria>
     </template>
     <template v-if="menu==2">
         <h1>Contenido menu 2</h1>
